d9b368d9046400c42aaf03460a43f2e18d587f8f
youssef lab20-u <yousseflab20@gmail.com>
2025-12-10 11:34:04 +0100

    fixed your errer for run android in emualtor

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,4 +1,8 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+const root = path.resolve(__dirname, '..');
+const watchFolders = [root];
 
 /**
  * Metro configuration
@@ -6,6 +10,11 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders,
+  resolver: {
+    nodeModulesPaths: [path.resolve(root, 'node_modules')],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
