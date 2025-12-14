ac598abe6d5f7c29cbc5b0c49ae684e7c2027d1e
youssef lab20-u <yousseflab20@gmail.com>
2025-12-14 22:55:52 +0100

    appgred package react native safe area contenxt

--- frontend/src/service/storage.tsx
+++ frontend/src/service/storage.tsx
@@ -1,6 +1,5 @@
 import { MMKV } from "react-native-mmkv";
 
-// storage.ts temporary
 export const storage = {
     set: (key: string, value: string) => {
         console.log(`[storage] set ${key} = ${value}`);
